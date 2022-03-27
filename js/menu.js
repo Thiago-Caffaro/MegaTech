1da3e9f80052d5d54aee9faa56f5fad373a6f41a
Cogumelinho <96150887+Thiago-Caffaro@users.noreply.github.com>
2022-03-27 20:48:14 -0300

    Botão para mobile adicionado

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,10 @@
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu() {
-    const nav = document.getElementById('menu');
+    const nav = document.getElementById('nav');
+    const main = document.getElementById('main');
     nav.classList.toggle('active');
+    main.classList.toggle('active')
 }
 
 btnMobile.addEventListener('click', toggleMenu);
